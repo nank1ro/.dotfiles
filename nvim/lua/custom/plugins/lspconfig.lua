6f03d41b9328779196098352c4be33860d2db33c
Alexandru Mariuti <alexandrumariuti@protonmail.com>
2022-12-11 11:40:36 +0200

    exclude flutter libs analysis

--- nvim/lua/custom/plugins/lspconfig.lua
+++ nvim/lua/custom/plugins/lspconfig.lua
@@ -2,6 +2,8 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 local merge_tb = vim.tbl_deep_extend
 local lspconfig = require "lspconfig"
+local utils = require "custom.utils"
+
 local json_capabilities = capabilities
 json_capabilities.textDocument.completion.completionItem.snippetSupport = true
 
@@ -25,6 +27,18 @@ local servers = {
       outline = true,
       suggestFromUnimportedLibraries = true,
     },
+    settings = {
+      dart = {
+        completeFunctionCalls = true,
+        showTodos = true,
+        renameFilesWithClasses = "prompt",
+        analysisExcludedFolders = {
+          utils.path_join(utils.flutter_sdk_path(), "packages"),
+          utils.path_join(utils.flutter_sdk_path(), ".pub-cache"),
+          utils.path_join(utils.home_path, ".pub-cache"),
+        },
+      },
+    },
   },
   -- swift
   sourcekit = {
@@ -41,8 +55,10 @@ local servers = {
   gopls = {},
 }
 
-
 for server_name, server_options in pairs(servers) do
   local merged_server_options = merge_tb("force", default_server_options, server_options)
+  if server_name == "dartls" then
+    vim.pretty_print(merged_server_options)
+  end
   lspconfig[server_name].setup(merged_server_options)
 end
--- nvim/lua/custom/utils.lua
+++ nvim/lua/custom/utils.lua
@@ -10,6 +10,26 @@ function M.git_repo_root_path()
   return vim.fn.system "git rev-parse --show-toplevel | tr -d '\n'"
 end
 
+-- Returns the flutter sdk path
+function M.flutter_sdk_path()
+  return vim.fn.system "which flutter | tr -d '\n'"
+end
+
+local uname = vim.loop.os_uname()
+local is_windows = uname.version:match "Windows"
+local path_sep = is_windows and "\\" or "/"
+
+---Join path segments using the os separator
+---@vararg string
+---@return string
+function M.path_join(...)
+  local result = table.concat(vim.tbl_flatten { ... }, path_sep):gsub(path_sep .. "+", path_sep)
+  return result
+end
+
+-- The user home path, e.g. /Users/alex
+M.home_path = os.getenv "HOME"
+
 -- Returns true if the file with the given [name] exists
 function M.file_exists(name)
   local f = io.open(name, "r")
