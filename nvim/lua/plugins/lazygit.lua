9203e7092e9c3923e452a317f27bee66722732c2
Alexandru Mariuti <alex@mariuti.com>
2025-12-18 15:18:31 +0100

    make lazygit fullscren

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    -- Make lazygit fullscreen
+    opts = {
+      styles = {
+        lazygit = {
+          height = 0,
+          width = 0,
+        },
+      },
+    },
+  },
+}
