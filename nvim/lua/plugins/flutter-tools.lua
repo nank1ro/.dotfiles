fba7fe7f3e5b52dabcd24ac1b090ea2b71181b35
Alexandru Mariuti <alex@mariuti.com>
2025-12-19 18:36:03 +0100

    update flutter  tools

--- nvim/lua/plugins/flutter-tools.lua
+++ nvim/lua/plugins/flutter-tools.lua
@@ -13,8 +13,8 @@ return {
         evaluate_to_string_in_debug_views = true,
       },
       dev_log = {
-        enabled = false,
-        open_cmd = "botright 15split", -- command to use to open the log buffer
+        enabled = true,
+        open_cmd = "edit", -- command to use to open the log buffer
         focus_on_open = false, -- focus on the newly opened log window
       },
       lsp = {
