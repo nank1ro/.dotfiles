bc4e0f2e1061232ff23393db4a7e314feb4692b1
Alexandru Mariuti <alex@mariuti.com>
2026-01-16 16:22:54 +0100

    add extras for languages

---
+++ nvim/lua/plugins/extras/lang.lua
@@ -0,0 +1,12 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.svelte" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.astro" },
+}
