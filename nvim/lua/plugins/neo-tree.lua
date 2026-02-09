9d935a9c4bea29a4bc3135f61fc09fb3327406ed
Alexandru Mariuti <alex@mariuti.com>
2026-02-09 15:42:19 +0100

    set hide_dotfiles to false

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -60,6 +60,7 @@ return {
       sources = { "filesystem", "buffers", "git_status" },
       open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
       filesystem = {
+        hide_dotfiles = false,
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
