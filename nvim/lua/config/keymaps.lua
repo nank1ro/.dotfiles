502b68c8d3755bff14efaae1ecd589e9a2138eef
Alexandru Mariuti <alex@mariuti.com>
2026-03-18 15:13:06 +0100

    fix/terminal toggle in lazyvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,3 +6,17 @@ if vim.g.vscode then
 end
 local map = vim.keymap.set
 map({ "i", "x", "n", "s" }, "<Char-0xAA>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Use the git root with a stable fallback to nvim's cwd.
+local function open_terminal()
+  local git_root = vim.fs.find(".git", { path = vim.uv.cwd(), upward = true })[1]
+  local cwd = git_root and vim.fn.fnamemodify(git_root, ":h") or vim.uv.cwd()
+  Snacks.terminal(nil, { cwd = cwd })
+end
+local function hide_terminal()
+  vim.cmd("hide")
+end
+map("n", "<c-/>", open_terminal, { desc = "Terminal (Root Dir)" })
+map("n", "<c-_>", open_terminal, { desc = "which_key_ignore" })
+map("t", "<c-/>", hide_terminal, { desc = "Hide Terminal" })
+map("t", "<c-_>", hide_terminal, { desc = "which_key_ignore" })
